bb44ea91787591b394b123013b87c54ad2c893bb
mtlaguerre <245204912+mtlaguerre@users.noreply.github.com>
2025-12-21 23:41:47 -0500

    projects section complete
    terminal changes:
    updated commands to be uniform with windows command prompt (because the terminal prompt is windows styled)
        - clear -> cls
        - ls -> dir
    
    new commands:
    run - opens the specified project in a new tab
    mkdir - for external users (secret use)
    
    updated commands:
    cd - for external users (secret use)

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const baseUrl = "https://api.github.com/";
 const users = ['iMarcraft', 'mtlaguerre'];
+const externalUsers = [];
 
 let username = users[0];
 
 let repos = [];
-let numOfRepos = 2; // Number of repositories to display
+let numOfRepos = 6; // Number of repositories to display
 let firstTimeToggle = true;
 
 // terminal variables
@@ -17,14 +18,8 @@ const welcomeMsg = `Welcome to my portfolio [Version ${version}]`;
 let currentInput = null;
 
 // define terminal paths
-const path = {
-    'C:' : {
-        Users : buildUsersDir(users)
-    },
-    'D:' : {
-        Users : {}
-    }
-};
+let path;
+buildPath();
 
 function buildUsersDir(usersArray) {
     const usersDir = {};
@@ -48,9 +43,9 @@ let cwd = path[drive];
 buildDefaultPrompt();
 
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     
-    
+    repos = await findUserRepos(username);  // find default user's repos
     buildProjectCards();
     
     // prevent form submit reloads
@@ -74,10 +69,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }, terminalLoadTime * 1.5)
 });
 
-function buildProjectCards() {
+async function buildProjectCards() {
     
     for (let i = 0; i < numOfRepos; i++) {
-        fetchUserRepo(baseUrl + 'users/' + username + '/repos', i);
+        const [repoInfo, repoImage] = await fetchUserRepo(baseUrl + 'users/' + username + '/repos', i);
+
+        // hide all github hosted websites
+        if (repoInfo.name.includes('github.io')) continue;
+
+        addProjectCard(repoInfo, repoImage);    // build project card
     }
 
 }
@@ -93,45 +93,52 @@ async function fetchUserRepo(url, index) {
         throw new Error(`HTTP error! status: ${response.status}`);
     
     let data = await response.json();
-    repoInfo = data[index];    
+    repoInfo = data[index];
     
     // Fetch Repo Image from README.md
     response = await fetch(baseUrl + 'repos/' + username + '/' + repoInfo.name + '/readme');
-    if (!response.ok)
+    if (response.ok) {
+        data = await response.json();
+        const readmeContentUrl = data.download_url; // Direct link to raw README content
+        
+        const markdownResponse = await fetch(readmeContentUrl);
+        const markdown = await markdownResponse.text();
+        
+        const regex = /!\[.*\]\((.*?)\)/; // Markdown image pattern
+        const match = markdown.match(regex);
+        
+        if (match && match[1]) {
+            repoImage = match[1];
+        }
+    }
+    else if (response.status === 404) {} // do nothing (the readme is missing)
+    else
         throw new Error(`HTTP error! status: ${response.status}`);
     
-    data = await response.json();
-    const readmeContentUrl = data.download_url; // Direct link to raw README content
-    
-    const markdownResponse = await fetch(readmeContentUrl);
-    const markdown = await markdownResponse.text();
-    
-    const regex = /!\[.*\]\((.*?)\)/; // Markdown image pattern
-    const match = markdown.match(regex);
-    
-    if (match && match[1]) {
-        repoImage = match[1];
-    }
-    
-    addProjectCard(repoInfo, repoImage);
+    return [repoInfo, repoImage]
 }
 
 function addProjectCard(repoInfo, repoImage) {
     
     let card = document.createElement('div');
+    const noDescription = "No discription yet. I'm sure they're considering it.\n -Marcus";
     
     card.className = 'card my-2 position-relative';
     card.style.width = '30%';
     card.innerHTML = 
-    `<img src="${repoImage}" alt="...">
+    `<img src="${repoImage ?? ''}" alt="${repoInfo.name} thumbnail">
     <div class="card-body">
     <h5 class="card-title">${repoInfo.name}</h5>
-    <p class="card-text pb-4">${repoInfo.description}</p>
+    <p class="card-text pb-4">${repoInfo.description ?? noDescription}</p>
     <a href="${repoInfo.html_url}" class="btn btn-primary position-absolute start-0 bottom-0 ms-3 mb-2">Go to repo</a>
     </div>
     `;
     
     document.getElementById('projects-section').appendChild(card);
+}
+
+function clearProjectCards() {
+    document.getElementById('projects-section').innerHTML = ''; // remove all children elements
 }
 
 function toggleProjectView() {
@@ -150,7 +157,7 @@ function toggleProjectView() {
     }
 }
 
-function displayInTerminal(text, isPrompt, typeSpeed = 0) {
+function displayInTerminal(text, isPrompt = false, typeSpeed = 0) {
     
     let parentElem = document.createElement('div');
     parentElem.className = 'd-flex position-relative align-items-start flex-wrap mb-1';
@@ -180,93 +187,201 @@ function displayInTerminal(text, isPrompt, typeSpeed = 0) {
         // scrollToBottom();
         
         // Handle Enter: echo command and create new prompt line
-        inputField.addEventListener('keydown', (e) => {
+        inputField.addEventListener('keydown', async (e) => {
             const sel = window.getSelection();
             const caretPos = sel.focusOffset;
             
             validCommands = {
-                'clear' : {
+                'cls' : {
                     argSize : 0,
-                    action : () => {
-                        terminal.innerHTML = ''; // clear terminal
+                    action : (args) => {
+                        // if no arguments
+                        if (!args)
+                            terminal.innerHTML = ''; // clear terminal
+                        else
+                            displayInTerminal('too many arguments');
                     }
                 },
                 'echo' : {
                     argSize : 1,
-                    action : (arg) => {
+                    action : (args) => {
                         let output;
-                        if (arg && arg !== null) {
-                            output = arg.replaceAll('"', '');   // remove double quotes
+                        // if args[0] has a value
+                        if (args && args[0] !== null) {
+                            output = args[0].replaceAll('"', '');   // remove double quotes
                         }
                         else {
                             output = '';
                         
                         }
-                        displayInTerminal(output, false);
+                        displayInTerminal(output);
                     }
                 },
-                'ls' : {
+                'dir' : {
                     argSize : 1,
-                    action : async (arg) => {
-                        if (arg) {
-                            if (isDirectory) {
-                                displayInTerminal('Not a directory', false);
+                    action : async (args) => {
+                        // if arguments exist
+                        if (args) {
+                            // if 1 argument
+                            if (args.length === 1) {
+                                // if args[0] not valid directory
+                                if (!isDirectory(cwd[args[0]])) {
+                                    displayInTerminal(`'${args[0]}' is not a directory`);
+                                }
+                                else {
+                                    let entries = Object.keys(cwd[args[0]]);
+
+                                    // if arguement equals 'Projects'
+                                    if (args[0] === 'Projects') {
+                                        await displayProjects(username, entries);
+                                    }
+
+                                    displayInTerminal(entries.join(' '));
+                                }
                             }
-                            else {
-                                let entries = Object.keys(cwd);
-                                displayInTerminal(entries.join(' '), false);
-                            }
+                            else    // otherwise, not 1 argument
+                                displayInTerminal('too many arguments');
                         }
-                        else {
-                            let entries = Object.keys(cwd);
+                        else {  // otherwise, no arguments
+                            let entries = Object.keys(cwd);     // sub directories
                             
                             // if in projects
                             if (dir[dir.length-1] == 'Projects') {
-                                repos = await findUserRepos(dir[1]);      // find all of user's public repos
-                                
-                                repos.forEach(repo => {
-                                    entries.push(repo.name);
-                                })
+                                await displayProjects(username, entries);
                             }
 
-                            displayInTerminal(entries.join(' '), false);
+                            displayInTerminal(entries.join(' '));
 
                         }
                     }
                 },
                 'cd' : {
-                    argSize : 1,
-                    action : (arg) => {
-                        if (arg) {
-                            if (arg === '..') {
-                                // if not at drive path
-                                if (dir.length > 0) {
-                                    // go back one directory
-                                    removeFromPrompt();
+                    argSize : 2,
+                    action : (args) => {
+                        // if arguments exist
+                        if (args) {
+                            // if 1 argument
+                            if (args.length === 1) {
+
+                                if (args[0] === '..') {
+                                    // if not at drive path
+                                    if (dir.length > 0) {
+                                        // go back one directory
+                                        removeFromPrompt();
+        
+                                        cwd = resolveCwd(); // update current working directory pointer
+                                    }
+                                }
+                                else if (args[0] === '/D') {    // if the only argument is '/D'
+                                    displayInTerminal('missing argument');
+                                }
+                                else {  // otherwise, args[0] != '..'
+                                    // if arg[0] is valid directory
+                                    if (isDirectory(cwd[args[0]])) {
     
-                                    cwd = resolveCwd(); // update current working directory pointer
+                                        // if at "Users" directory"
+                                        if (dir.length === 1){
+                                            username = args[0]; // update username
+                                            
+                                            // rebuild project cards
+                                            clearProjectCards();
+                                            buildProjectCards();
+                                        }
+    
+                                        // go into directory
+                                        addToPrompt(args[0]);
+    
+                                        cwd = resolveCwd();
+    
+                                    }
+                                    else    // otherwise, args[0] invalid
+                                        displayInTerminal("'" + args[0] + "' directory not found");
                                 }
                             }
-                            else {
-                                if (isDirectory(cwd[arg])) {
-                                    // go into directory
-                                    addToPrompt(arg);
-
-                                    cwd = resolveCwd();
-
+                            else if (args.length === 2) {
+                                // if first arg equals '/D' (change drive)
+                                if (args[0] === '/D') {
+                                    // if drive exists on system (within paths)
+                                    if (Object.keys(path).includes(args[1])) {
+                                        drive = args[1];   // dynamically change to valid drive
+                                        dir = [];       // clear directories array
+                                        cwd = path[drive];  // update current directory pointer
+                                        buildPrompt(drive, dir);
+                                    }
+                                    else
+                                        displayInTerminal('drive not found');
                                 }
                                 else
-                                    displayInTerminal("'" + arg + "' directory not found", false);
+                                    displayInTerminal('invalid argument(s)');
                             }
+                            else    // otherwise, unhandled amount of arguments
+                                displayInTerminal('too many arguments');
+                        }
+                        else    // otherwise, no arguments
+                            displayInTerminal('missing arguments');
+                    }
+                },
+                'run' : {
+                    argSize : 1,
+                    action : (args) => {
+                        // if arguments exist
+                        if (args) {
+                            // if 1 argument
+                            if(args.length === 1) {
+
+                                foundRepo = false;
+                                repoName = '';          // github repo name placeholder
+
+                                repos.forEach(repo => {
+                                    console.log(repo.name);
+                                    // if args[0] matches repo name
+                                    if (args[0] === repo.name){
+                                        foundRepo = true;
+                                        repoName = repo.name;
+                                    }
+                                })
+    
+                                if (foundRepo) {
+                                    window.open(`https://github.com/${username}/${repoName}`);
+                                }
+                                else {
+                                    displayInTerminal('Repo not found... :(');
+                                }
+                            }
+                            else
+                                displayInTerminal('too many arguments');
                         }
                         else
                             displayInTerminal('missing arguments');
                     }
                 },
-                'cat' : {
+                'mkdir' : {
                     argSize : 1,
-                    action : (arg) => {
-                        
+                    action : (args) => {
+                        // if at "Users" directory of D: drive
+                        if (dir.length == 1 && drive !== Object.keys(path)[0] ) {
+                            if (args) {
+                                // if 1 argument
+                                if (args.length === 1) {
+
+                                    // add user to user list
+                                    externalUsers.push(args[0]);
+    
+                                    // recreate path
+                                    buildPath();
+                                    console.log('path: ', path);
+    
+                                    // update path variables
+                                    cwd = path[drive]['Users'];
+                                }
+                                else
+                                    displayInTerminal('too many arguments');
+                            }
+                            else
+                                displayInTerminal('missing arguments');
+                        }
+                        else
+                            displayInTerminal('permissions denied');
                     }
                 }
             }
@@ -293,7 +408,7 @@ function displayInTerminal(text, isPrompt, typeSpeed = 0) {
                     }
                     
                     // console.log('args: ', args);
-                    cmd.action(args[cmd.argSize > 0 ? cmd.argSize - 1 : null]);   // perform command action with passed arguments
+                    await cmd.action(args);   // perform command action with passed arguments
                 }
                 else {
                     
@@ -308,16 +423,14 @@ function displayInTerminal(text, isPrompt, typeSpeed = 0) {
                     
                     let args = splitArgs(rawInput);
                     if (rawInput)
-                        displayInTerminal(`command: '${args[0]}' not found.`, false);
+                        displayInTerminal(`command: '${args[0]}' not found.`);
                     
                     currentInput = null;
                 }
                 
                 // Create a new prompt line
-                setTimeout(() => {
-                    displayInTerminal(prompt, true);
-                    scrollToBottom();
-                }, 0);
+                displayInTerminal(prompt, true);
+                scrollToBottom();
             }
             else if (e.key === ('Backspace') && caretPos <= text.length) {
                 e.preventDefault();
@@ -464,4 +577,23 @@ async function findUserRepos(user) {
     let data = await response.json();
     
     return data;
+}
+
+function buildPath() {
+    path = {
+        'C:' : {
+            Users : buildUsersDir(users)
+        },
+        'D:' : {
+            Users : buildUsersDir(externalUsers)
+        }
+    };
+}
+
+async function displayProjects(user, entries) {
+    repos = await findUserRepos(username);      // find all of user's public repos
+
+    repos.forEach(repo => {
+        entries.push(repo.name);        // replace subdirectories with public repos
+    })
 }

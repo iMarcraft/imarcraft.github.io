0c005e3d8bee881b123610d71116bd3cda1e79e1
mtlaguerre <245204912+mtlaguerre@users.noreply.github.com>
2025-12-25 18:47:05 -0500

    professionalized
    updated title display for mobile response
    updated about circle size
    updated form field placeholders
    
    reminder:
        version 0.1.12 commented ideas

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -1,6 +1,6 @@
 const baseUrl = "https://api.github.com/";
 const BACKEND_URL = "https://error-monitor.fly.dev";
-const version = "1.0.2";
+const version = "1.1.0";
 let theme = 'dark';
 
 const users = ['iMarcraft', 'mtlaguerre'];
